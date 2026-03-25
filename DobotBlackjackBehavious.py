49d3c1868e44a56db1cd49dca9c85dbd794cb6b5
blueberious1 <118052173+blueberious1@users.noreply.github.com>
2026-03-25 09:29:25 +0200

    Parem ja normaalse kiirusega

--- DobotBlackjackBehavious.py
+++ DobotBlackjackBehavious.py
@@ -1,5 +1,4 @@
 #Magician
-#Magician
 import time
 import math
 
@@ -12,36 +11,45 @@ def deltamove(x, y, z):
 	dType.SetPTPCmdEx(api, 7, x,  y,  z, 0, 1)
 
 def hitMe():
-	dType.SetPTPLParamsEx(api,500,1000,1)
-	deltamove(0,0,-40)
+	dType.SetPTPCoordinateParamsEx(api,500,1000,500,1000,1)
+	deltamove(0,0,-55)
+	dType.SetPTPCoordinateParamsEx(api,4000,8000,4000,8000,1)
 	for i in range(2):
-		deltamove(0,0,-20)
-		deltamove(0,0,20)
-	deltamove(0,0, 40)
+		deltamove(0,0,-10)
+		deltamove(0,0,10)
+	dType.SetPTPCoordinateParamsEx(api,500,1000,500,1000,1)
+	deltamove(0,0, 55)
 
 def stand():
-	dType.SetPTPLParamsEx(api,500,1000,1)
-	for i in range(2):
-		deltamove(20,20,0)
-		deltamove(-40,-40,0)
-		deltamove(20,20,0)
+	dType.SetPTPCoordinateParamsEx(api,500,1000,500,1000,1)
+	deltamove(40,0,0)
+	deltamove(-80,0,0)
+	deltamove(80,0,0)
+	deltamove(-80,0,0)
+	deltamove(40,0,0)
 
 def lose():
-	dType.SetPTPLParamsEx(api,20,50,1)
+	dType.SetPTPCoordinateParamsEx(api,200,100,200,100,1)
 	deltamove(0,0,-50)
-	time.sleep(2)
+	deltamove(20,0,0)
+	deltamove(-40,0,0)
+	deltamove(20,0,0)
+	time.sleep(1)
 	deltamove(0,0,50)
 
 def win():
-	dType.SetPTPLParamsEx(api,500,1000,1)
+	dType.SetPTPCoordinateParamsEx(api,1000,2000,1000,2000,1)
 	deltamove(0,0,50)
 	for i in range(3):
-		deltamove(20,20,20)
-		deltamove(-40,-40,0)
-		deltamove(20,20,-20)
+		deltamove(0,20,20)
+		deltamove(0,-40,0)
+		deltamove(0,20,-20)
 	time.sleep(1)
 	deltamove(0,0,-50)
 
-moveto(0, 0, 60)
-
-"""INSERT GAMEPLAY LOOP
+dType.SetPTPCoordinateParamsEx(api,500,2000,500,2000,1)
+moveto(0, -260, -0)
+hitMe()
+stand()
+lose()
+win()
