68438e278863535a6f55e633b0070e0336265665
LennartLuud <95574424+LennartLuud@users.noreply.github.com>
2026-04-01 08:58:14 +0300

    Add files via upload
    goat töötab, pole integreeritud dobotit

--- bj_gambler.py
+++ bj_gambler.py
@@ -1,5 +1,5 @@
+fail = "C:/Users/luud.lt7a493/Desktop/kaardid.txt"
 from time import sleep
-f = open("C:/Users/luud.lt7a493/Desktop/kaardid.txt")
 minul = 0 #summad
 temal = 0
 kord = "" #str: player, wait
@@ -7,6 +7,7 @@ p_eel = [0]
 d_eel = [0]
 eel_teg = ""
 otsus = ""
+killswitch = 0
 
 def sumo(nimekiri):
     ajut = 0
@@ -18,28 +19,32 @@ def sumo(nimekiri):
             if nimekiri[i]== "J" or nimekiri[i] == "Q" or nimekiri[i] =="K":
                 ajut += 10
             elif nimekiri[i] == "A":
-                ajut +=1
+                assad +=1
     if assad == 1:
-        if (ajut + 10 ) > 21:
+        if (ajut + 11 ) > 21:
             ajut += 1
         else:
-            ajut += 10
+            ajut += 11
     elif assad ==2:
         if (ajut + 1) > 21:
             ajut += 2
         else:
             ajut += 12
     elif assad >= 3:
-        ajut += assad
+        if (ajut + 11 + (assad-1)) <= 21:
+            ajut += (11+ (assad-1))
+        else:
+            ajut += assad
     return ajut
 
 def saa_seis(fail):
-    global minul, temal, kord
-    read = fail.readlines()
-    ma = read[0].strip().split(); ta = read[1].strip().split(); kord = read[2].strip()
+    global minul, temal, kord, p_eel, d_eel, killswitch
+    with open(fail, "r") as f:
+        read = f.readlines()
+    ma = read[0].strip().split(); ta = read[1].strip().split(); kord = read[2].strip(); killswitch = int(read[3].strip())
     ma.sort(), ta.sort()
     if p_eel == ma and d_eel == ta:
-        sleep(1000)
+        sleep(1);print("ootan, sama")
     else:
         minul = sumo(ma)
         temal = sumo(ta)
@@ -52,13 +57,32 @@ def kaik():
     if minul < 17:
         otsus = "hit"
         eel_teg = otsus
-    elif minul > 17 and minul <= 21:
+    elif minul >= 17 and minul <= 21:
         otsus = "stand"
         eel_teg = otsus
     elif minul > 21:
         otsus = "bust"
         eel_teg = otsus
 
-saa_seis(f)
-if kord == "player":
-    kaik()
+
+while killswitch == 0:
+    saa_seis(fail)
+    if kord == "wait":
+        sleep(3)
+        killswitch = 1
+    elif kord == "player":
+        kaik()
+        print(otsus)
+        if otsus == "stand":
+            kord = "wait"
+            killswitch = 1
+        elif otsus == "bust":
+            kord == "wait"
+            killswitch = 1
+            sleep(3)
+
+        elif kord == "wait":
+            sleep(3)
+            killswitch = 1
+
+print("tsau")
